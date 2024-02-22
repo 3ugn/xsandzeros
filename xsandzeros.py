7541445fa24069c93e9c6cdfa0b8a02d82957014
3ugn <160858759+3ugn@users.noreply.github.com>
2024-02-22 17:13:00 +0500

    Update xsandzeros.py

--- xsandzeros.py
+++ xsandzeros.py
@@ -1,3 +1,20 @@
-bitxch = open("bitxch.txt", "w+")
-bitxch.write('10')
-bitxch.close()
+doska = [[' ' for _ in range(3)] for _ in range(3)]
+def print_doska(doska):
+    for row in doska:
+        print(' | '.join(row))
+        print('---------')
+
+current_player = '1'
+while True:
+    print_doska(doska)
+    row = int(input(f'Игрок {current_player}, введите номер строки (0, 1 или 2): '))
+    col = int(input(f'Игрок {current_player}, введите номер столбца (0, 1 или 2): '))
+    if doska[row][col] == ' ':
+        doska[row][col] = current_player
+
+        if current_player == '1':
+            current_player = '2'
+        else:
+            current_player = '1'
+    else:
+        print('Клетка уже занята, выберите другую!')
